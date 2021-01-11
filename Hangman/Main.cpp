e56e4ed08e0bf3c8cbd7ee31f5ea8a39dc36d36b
Wither <wither.tweets@gmail.com>
2021-01-11 19:22:59 +0100

    Added some logic flow and helper functions

--- Hangman/Main.cpp
+++ Hangman/Main.cpp
@@ -7,12 +7,20 @@
 
 int lives;
 
+std::string Hangman::string_to_blank(const std::string& input) {
+	std::string output;
+	for (int i = 0; i < input.size(); i++) {
+		output += "_ ";
+	}
+	return output;
+}
+
 bool Hangman::str_contains_letter(const std::string& string, const char& letter) {
 	return string.find(letter) != std::string::npos;
 }
 
 int Hangman::random(int min, int max) {
-	return rand() % max;
+	return rand() % (max - min + 1) + min;
 }
 
 void Hangman::print(const char* message) {
@@ -27,7 +35,7 @@ void Hangman::load_words(std::vector<std::string>& words) {
 	if (world_file_stream.is_open()) {
 		std::string line;
 		while (std::getline(world_file_stream, line)) {
-			if (line[0] == '#') continue;
+			if (line[0] == '#' || line.empty()) continue;
 			words.push_back(line);
 		}
 		world_file_stream.close();
@@ -40,8 +48,12 @@ void Hangman::start() {
 	Hangman::load_words(words);
 	Hangman::print("Starting Hangman! Picking word...");
 
-	std::string selected_word = words[Hangman::random(0, words.size())];
-	Hangman::print(selected_word.c_str());
+	int random_index = Hangman::random(0, words.size());
+	std::string selected_word = words[random_index];
+	Hangman::print("Word selected!");
+    Hangman::print(Hangman::string_to_blank(selected_word).c_str());
+
+	lives = 5;
 
 	std::cin.get();
 }
