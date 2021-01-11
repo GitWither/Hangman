0bd8f6e814e2468005b68bf91ac1ea59c7991f00
Wither <wither.tweets@gmail.com>
2021-01-11 14:55:40 +0100

    Added support for comments in word files

--- Hangman/Main.cpp
+++ Hangman/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <math.h>
 
 #include "Main.h"
@@ -6,22 +7,41 @@
 
 int lives;
 
-const std::string WORLD_POOL[3] = {
-	"dfgdf",
-	"sdgfsdfg",
-	"sdfg"
-};
-
 bool Hangman::str_contains_letter(const std::string& string, const char& letter) {
 	return string.find(letter) != std::string::npos;
+}
+
+int Hangman::random(int min, int max) {
+	return rand() % max;
 }
 
 void Hangman::print(const char* message) {
 	std::cout << message << std::endl;
 }
 
+void Hangman::load_words(std::vector<std::string>& words) {
+	std::fstream world_file_stream;
+
+	world_file_stream.open("words.txt", std::ios::in);
+
+	if (world_file_stream.is_open()) {
+		std::string line;
+		while (std::getline(world_file_stream, line)) {
+			if (line[0] == '#') continue;
+			words.push_back(line);
+		}
+		world_file_stream.close();
+	}
+	else Hangman::print("Unable to load words");
+}
+
 void Hangman::start() {
+	std::vector<std::string> words;
+	Hangman::load_words(words);
 	Hangman::print("Starting Hangman! Picking word...");
+
+	std::string selected_word = words[Hangman::random(0, words.size())];
+	Hangman::print(selected_word.c_str());
 
 	std::cin.get();
 }
